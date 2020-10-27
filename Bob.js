a0a237e903fd3a6cae3c9d3a45330bbdb8dcf2db
Ashwika071106-rgb <72143078+Ashwika071106-rgb@users.noreply.github.com>
2020-10-27 12:33:52 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,35 @@
+class Bob{
+
+    constructor(x,y,radius){
+
+        var options = {
+            isStatic:false,
+            restitution:0.8,
+            friction:0.5,
+            density:1.2
+        }
+        //this.x=body.position.x;
+        //this.y=body.position.y
+        this.radius=radius;
+        this.body = Bodies.circle(x,y,this.radius/2,options);
+        //this.image = loadImage("sprites/paper1.png");
+        //this.x= body.position.x;
+        //this.y=body.position.y;
+        World.add(world, this.body);
+    }
+
+    display(){
+        var pos = this.body.position;
+      
+      //var angle = this.body.angle;
+      //push();
+      //translate(pos.x,pos.y ); 
+      
+      stroke(255);
+      strokeWeight(3);
+      fill(rgb(176,224,230));
+      rectMode(CENTER);
+      ellipse(pos.x,pos.y, this.radius,this.radius);
+      //pop();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# project27_newtonsCradle
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
